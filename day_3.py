cc28abca0e4b3e4d095ec776d7f3056b23921cf3
Ankita Mohanta <rakeshsaha87550@gmail.com>
2023-02-02 21:14:49 +0530

    escape sequence, comments, variabled, datatype

---
+++ day_3.py
@@ -0,0 +1,13 @@
+# comment amd escape sequence
+# this is another comment
+"""
+this is 
+a multiline comment
+"""
+print("hello my name is Rakesh Saha \nI am 23 years old");
+print("hello my name is\"Rakesh Saha\"\nI am 23 years old");
+# separator
+print("hey",2,4,sep="~");
+print("hey",2,4,sep="~", end="!!");
+
+
